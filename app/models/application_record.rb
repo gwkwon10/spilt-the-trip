ee43388ca44b6608b328e2ae25f116cf80e6a72a
Jack DeKeersgieter <dekeersgieter.3@osu.edu>
2025-04-09 04:28:38 -0400

    model apprec entries

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,47 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
+
+# class Table < ApplicationRecord
+#   attr_accessible :col, :col2, :col3
+# end
+
+# t = Table.[new/create] col: 'A', col2: 24
+# new: t is a Table object
+# create: t is Table obj, t added to db
+# t.destroy
+
+# t = Table.find 24   # by :id
+# t = Table.findby col: 'A'
+# t = Table.all   # or .first .last
+
+# t.col = 'B'
+# t.save to save changes to :col
+
+class User < ApplicationRecord
+  attr_accessible :username, :dislayName
+end
+
+class Authenticate < ApplicationRecord
+  attr_accessible :uid, :password
+end
+
+class Owes < ApplicationRecord
+  attr_accessible :uidOwes, :uidOwed, :amountOwed
+end
+
+class Trip < ApplicationRecord
+  attr_accessible :startDate, :endDate, :name, :ownerid, :defaultCurrency
+end
+
+class OnTrip < ApplicationRecord
+  attr_accessible :uid, :tid, :balance
+end
+
+class Expense < ApplicationRecord
+  attr_accessible :amount, :tid, :currency, :category, :date, :desc
+end
+
+class Liable < ApplicationRecord
+  attr_accessible :uid, :amountLiable, :eid
+end
