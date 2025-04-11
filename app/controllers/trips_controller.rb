9af364e3d31d49ebded42a0922a971abf90367ff
Gee-Woo Kwon <kwon.539@osu.edu>
2025-04-10 23:52:58 -0400

    add more actions to trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,32 @@
 class TripsController < ApplicationController
-  #Show specific trip page
-  def specific
+  # main index page with all the trips of the user
+  def index
+    @trips = Trip.all
+  end
+
+  # Show specific trip page
+  def show
+    @trip = Trip.find(params[:id])
+    @expenses = @trip.expenses
+  end
+
+  # request for new trip to be added
+  def new
+    @trip = Trip.new
+  end
+
+  # add new trip to the database
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to @trip, notice: "Trip created sucessfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def trip_params
+    params.require(:trip).permit(:title) #add other parameters
   end
 end
