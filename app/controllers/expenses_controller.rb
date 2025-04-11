25e943bbe8c7cdb565fedbac0ebee286b6066d97
Gee-Woo Kwon <kwon.539@osu.edu>
2025-04-10 22:38:18 -0400

    adding new expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,12 +1,26 @@
 class ExpensesController < ApplicationController
   # Get new expenses
   def new
+    @trip = Trip.find(params[:trip_id])
     @expense = @trip.expenses.new
   end
 
   # Add new expenses
-  def add
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @expense = @trip.expenses.new(expense_params)
+    if @expense.save
+      @trip.recalulate_expenses
+      redirect_to trip_path(@trip), notice: "Expense added successfully!"
+    else
+      render :new
+    end
   end
 
+  private
+
+  def expense_params
+    params.require(:expense).permit(:description, :amount, :date, :traveler_id, traveler_ids: [])
+  end
 
 end
