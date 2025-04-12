6ed761bbb8b1884086b0b194e6108a7c2d0397c4
Jack DeKeersgieter <dekeersgieter.3@osu.edu>
2025-04-12 15:49:17 -0400

    balance moved to liable

--- db/migrate/20250409092545_create_liables.rb
+++ db/migrate/20250409092545_create_liables.rb
@@ -2,6 +2,7 @@ class CreateLiables < ActiveRecord::Migration[8.0]
   def change
     create_table :liables do |t|
       t.float :amountLiable
+      t.float :paid
 
       t.belongs_to :user
       t.belongs_to :expense
