d83fbe6749573ad9bedd765dbc317ae4283fa550
Gee-Woo Kwon <kwon.539@osu.edu>
2025-04-13 22:46:30 -0400

    added to expense form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find_by(id: sesssion[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def user_login?
