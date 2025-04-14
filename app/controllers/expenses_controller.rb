f0a30d2a5e523d9308de40968f55aa290b20acf7
Jack DeKeersgieter <dekeersgieter.3@osu.edu>
2025-04-14 00:13:25 -0400

    javaowe creation and ruby syntax

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -26,54 +26,60 @@ class ExpensesController < ApplicationController
 
   def calc_owes
     # go through each expense, find deltas in owes for each liable in the expense
-    exArr = Expense.all
+    ex_arr = Expense.all
     # amount MUST equal sum(amountLiable)
-    for (int i=0; i<exArr.length; i++) {
-      lbArr = exArr[i].liables # arr of liables in some ex
-      amt = exArr[i].amount #current ex cost
-      userOwesArr[] = {}
-      userOwedArr[] = {}
+    ex_arr.each do |expense|
+      lb_arr = expense.liables # arr of liables in some ex
+      amt = expense.amount #current ex cost
+      user_owes_arr = []
+      user_owed_arr = []
       total_paid = 0
       total_debt = 0
       users_owed = 0
       users_owing = 0
 
-      for (int j=0; j<lbArr.length; j++) {
-        total_paid += lbArr[j].paid
-        net = lbArr[j].paid-lbArr[j].amountLiable # +owed money, negative=owes money
-        if (net < 0) {
-          users_owing++
-          userOwesArr.push({j,net})
+      lb_arr.each_with_index do |liable, i|
+        total_paid += liable.paid
+        net = liable.paid-liable.amountLiable # +owed money, negative=owes money
+        if net < 0
+          users_owing += 1
+          user_owes_arr < [i,net]
           
-        }
-        if (net > 0) {
-          users_owed++
-          userOwedArr.push({j,net})
+        end
+        if net > 0
+          users_owed += 1
+          user_owed_arr < [i,net]
           total_debt+=net
-        } # if 0, that person is neither owed nor owes
-      }
-# - ) P _ 0 p
-      if (total_paid==amt) { # then, debts can be accurately calculated
-        for (int j=0; j<userOwesArr.length; j++) {
-          debt_ratio = userOwesArr[j][1]/total_debt #percentage of debt they must pay, which is the percentage per Owed
-          for (int k=0; k<userOwedArr.length; k++) {
-            uid1 = lbArr[userOwesArr[j][0]].user_id
-            uid2 = lbArr[userOwedArr[k][1]].user_id
-            if (uid1 > uid2) {
+        end # if 0, that person is neither owed nor owes
+      end
+
+      if total_paid == amt # then, debts can be accurately calculated
+        user_owes_arr.each do |owes_i, owes_amt|
+          debt_ratio = owes_amt.to_f / total_debt #percentage of debt they must pay, which is the percentage per Owed
+          user_owed_arr.each do |owed_i, owed_amt|
+            uid1 = lb_arr[owes_i].user_id
+            uid2 = lb_arr[owed_i].user_id
+            amtOwed = debt_ratio * owed_amt # amount that user_owes_arr[j] owes user_owed_arr[k]
+            if uid1 > uid2 #sort uids, now Owe(uid2,uid1) will never exist, only Owe(uid1,uid2)
               temp = uid2
               uid2 = uid1
               uid1 = temp
-            }
-            amtOwed = debt_ratio*userOwedArr[k][1] # amount that userOwesArr[j] owes userOwedArr[k]
-            # if 
-            # if lbArr[userOwesArr[j][0]].user_id and lbArr[userOwedArr[k][1]].user_id have an Owe together, O.amountOwed+=amtOwed
-            owe = Owe.findby(userOwed_id userOwing_id)
+              amtOwed *= -1 # delta for 1 to 2 Owe
+            end
+            
+            owe = Owe.findby(userOwing: uid1,userOwed: uid2)
+            
             # else, create an Owe between them and set O.amountOwed = amtOwed
-          }
-        }
-      }
-    }
-    
+            if owe # if uid1 and uid2 have an Owe together, O.amountOwed+=amtOwed
+              owe.amountOwed += amtOwed #if amountOwed negative, Owed owes Owing. If aO +, Owing owes Owed
+              owe.save
+            else
+              Owe.create(userOwing: uid1,userOwed: uid2, amountOwed: amtOwed)
+            end
+          end
+        end
+      end
+    end
   end
 
 end
