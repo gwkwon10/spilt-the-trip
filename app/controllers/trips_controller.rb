4f424a0a4b3e7fe11d0ce0720869aa721b62e8e3
taekim3 <115376951+taekim3@users.noreply.github.com>
2025-04-14 05:43:49 -0400

    select participants

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -19,38 +19,54 @@ class TripsController < ApplicationController
   # request for new trip to be added
   def new
     @trip = Trip.new
+    @users = User.where.not(id: current_user.id)
   end
 
   # add new trip to the database
   def create
     @trip = Trip.new(trip_params)
-    @trip[:ownerid] = current_user.id
+    @trip.ownerid = current_user.id
     if @trip.save
       Rails.logger.debug("Made trip")
       # create user trip relation for current user
-      OnTrip.create(user_id: current_user.id, trip_id: @trip.id)
-      Rails.logger.debug("Added current user")
-      # find the user for each traveler added to the trip
-      if params[:traveler_emails].present?
-        Rails.logger.debug("Access emails")
-        traveler_emails = params[:traveler_emails].split(",").map(&string)
-        traveler_emails.each do |email|
-          user = User.find_by(email: email)
-          if user
-            OnTrip.create(user_id: user.id, trip_id: @trip.id, balance: 0)
-          end
-          # potentially figure out how to handle if user is not signed up to the application
-        end
-      end
-      redirect_to @trip
-    else
-      flash.now[:alert] = @trip.errors.full_messages.to_sentence
-      render :new
+      OnTrip.create(user_id: current_user.id, trip_id: @trip.id, balance: 0)
+  # Rails.logger.debug("Added current user")
+  # find the user for each traveler added to the trip
+  #     if params[:traveler_emails].present?
+  #       Rails.logger.debug("Access emails")
+  #       traveler_emails = params[:traveler_emails].split(",").map(&string)
+  #       traveler_emails.each do |email|
+  #         user = User.find_by(email: email)
+  #         if user
+  #           OnTrip.create(user_id: user.id, trip_id: @trip.id, balance: 0)
+  #         end
+  #         # potentially figure out how to handle if user is not signed up to the application
+  #       end
+  #     end
+  #     redirect_to @trip
+  #   else
+  #     flash.now[:alert] = @trip.errors.full_messages.to_sentence
+  #     render :new
+  #   end
+  # end
+  #
+  #
+  if params[:participant_ids].present?
+     params[:participant_ids].each do |user_id|
+    OnTrip.create(user_id: user_id, trip_id: @trip.id, balance: 0)
     end
   end
 
+    redirect_to @trip
+    else
+    flash.now[:alert] = @trip.errors.full_messages.to_sentence
+    @users = User.where.not(id: current_user.id) # repopulate for render :new
+    render :new
+    end
+   end
+
   private
   def trip_params
-    params.require(:trip).permit(:name, :startDate, :endDate, :defaultCurrency) #add other parameters
+    params.require(:trip).permit(:name, :startDate, :endDate, :defaultCurrency) # add other parameters
   end
 end
