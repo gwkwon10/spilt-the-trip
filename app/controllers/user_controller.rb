bb9fdab91472134aa9dc4c69de9e0806518a8f2d
Gee-Woo Kwon <kwon.539@osu.edu>
2025-04-14 01:02:24 -0400

    debug sign-up error with no flash alert

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to landing_path
+      redirect_to root_path
     else
       flash.now[:alert] = @user.errors.full_messages.to_sentence
       render :new
