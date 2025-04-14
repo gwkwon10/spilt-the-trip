7df1f1019c50ada487e94bfffbad2c67ebde187e
Gee-Woo Kwon <kwon.539@osu.edu>
2025-04-13 20:19:29 -0400

    adding trip to database

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,14 +1,18 @@
 class TripsController < ApplicationController
   # main index page with all the trips of the user
   def index
-    @trips = Trip.all
+    if session[:user_id]
+      @trips = current_user.trips
+    else
+      redirect_to login_path, alert: "Please log in first"
+    end
   end
 
   # Show specific trip page
   def show
     @trip = Trip.find(params[:id])
     @expenses = @trip.expenses
-    @travelers = @trip.on_trip
+    @travelers = @trip.travelers
   end
 
   # request for new trip to be added
@@ -21,6 +25,14 @@ class TripsController < ApplicationController
     @trip = Trip.new(trip_params)
     @trip.ownerid = current_user.id
     if @trip.save
+      OnTrip.create(user_id: current_user.id, trip_id: @trip.id)
+      if params[:traveler_emails].present?
+        traveler_emails = params[:traveler_emails].spilt(",").map(&string)
+        users = User.where(email: traveler_emails)
+        users.each do |user|
+          OnTrip.create(user_id: user.id, trip_id: @trip.id)
+        end
+      end
       redirect_to @trip, notice: "Trip created sucessfully!"
     else
       render :new
@@ -29,6 +41,6 @@ class TripsController < ApplicationController
 
   private
   def trip_params
-    params.require(:trip).permit(:startDate, :endDate, :name, :defaultCurrency) #add other parameters
+    params.require(:trip).permit(:name, :startDate, :endDate, :defaultCurrency) #add other parameters
   end
 end
