9d3deec512188e36492360c131d2629eb4c08124
Jack DeKeersgieter <dekeersgieter.3@osu.edu>
2025-04-14 03:47:49 -0400

    logging out

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "session#new"
   post "/login", to: "session#create"
-  delete "/logout", to: "session#destroy"
+  delete '/logout', to: 'session#destroy'
 
 
   resources :trips, only: [ :index, :show, :new, :create ] do
