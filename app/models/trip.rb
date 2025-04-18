bd09103e665b09f5544e72b737419e3ab12657fc
Gee-Woo Kwon <kwon.539@osu.edu>
2025-04-18 02:23:59 -0400

    catch add trip error

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,15 @@ class Trip < ApplicationRecord
     has_many :on_trips, dependent: :destroy
     has_many :users, through: :on_trips
     has_many :expenses, dependent: :destroy
+
+    validate :proper_dates
+
+    private
+    def proper_dates
+      if trip.startDate > trip.endDate
+        errors.add(:date, "Start Date: #{trip.startDate} must started before #{trip.endDate}")
+      end
+    end
     # here we put methods
     #Pp0_)-
 end
