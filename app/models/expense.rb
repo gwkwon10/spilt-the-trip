93a93bab1bf8c57f76d0c2e0ba3f5d98223f5ab5
Gee-Woo Kwon <kwon.539@osu.edu>
2025-04-18 02:10:21 -0400

    catch error for expense form

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,14 @@ class Expense < ApplicationRecord
     belongs_to :trip
     has_many :liables, dependent: :destroy
     has_many :users, through: :liables
+
+    validate :date_within_trip
+
+    private
+    def date_within_trip
+      if date < trip.startDate || date > trip.endDate
+        errors.add(:date, "must be within the trip dates (#{trip.startDate} to #{trip.endDate})")
+      end
+    end
+
 end
